768a1f5fc33ddf8cbba72678b5fba9dbe570a339
yucl80 <ycl123>
2017-08-25 10:56:19 +0800

    init

--- avro2parquet-spark/src/main/scala/yucl/learn/demo/log2hdfs/AvroIOScalaDemo.scala
+++ avro2parquet-spark/src/main/scala/yucl/learn/demo/log2hdfs/AvroIOScalaDemo.scala
@@ -1,6 +1,5 @@
 package yucl.learn.demo.log2hdfs
 
-import com.databricks.spark.avro._
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -19,9 +18,10 @@ object AvroIOScalaDemo {
       (2017, 7, "/test/b.htm", 404),
       (2017, 7, "/test/c.htm", 500)).toDF("year", "month", "uri", "code")*/
 
-    sqlContext.setConf("spark.sql.avro.compression.codec", "deflate")
-
-    val filePath = "hdfs://10.62.14.46:9000/logs/acclog"
+    //sqlContext.setConf("spark.sql.avro.compression.codec", "deflate")
+    //sqlContext.setConf("mapreduce.input.fileinputformat.split.maxsize","67108864")
+    sc.hadoopConfiguration.set("mapreduce.input.fileinputformat.split.maxsize","134217728")
+    val filePath = "hdfs://10.62.14.67:8020/logs/acclog"
 
     // val name = "AvroTest"
     // val namespace = "yucl.learn.demo"
@@ -30,13 +30,18 @@ object AvroIOScalaDemo {
 
    /* df.write.mode(SaveMode.Append).partitionBy("year", "month").avro(filePath)*/
 
-    /* sqlContext.sql("CREATE TEMPORARY TABLE table_name USING com.databricks.spark.avro OPTIONS (path \"" + filePath + "\")")
-    val df2 = sqlContext.sql("SELECT * FROM table_name where service='gqs' and stack='it-dev-20170824'")
-    df2.foreach { x => println(x) }*/
+    sqlContext.sql("CREATE TEMPORARY TABLE table_name USING com.databricks.spark.avro OPTIONS (path \"" + filePath + "\")")
+    val df2 = sqlContext.sql("SELECT * FROM table_name where service='um' and stack='it-dev-20170810' and sessionid='19551B3CDE72D9B0B2E8080AF438DFA1'")
+    df2.foreach { x => println(x) }
 
-   val df1 = sqlContext.read.avro(filePath)
+  /* val df1 = sqlContext.read.avro(filePath)
     df1.printSchema()
 
-    df1.filter("year = 2017" ).collect().foreach(println)
+    df1.filter("year = 2017" )
+      .filter("service = 'um'")
+      .filter("stack = 'it-dev-20170810'")
+      .filter("sessionid  = '19551B3CDE72D9B0B2E8080AF438DFA1'")
+      .collect()
+      .foreach(println)*/
   }
 }
